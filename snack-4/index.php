e9487f1fc0851e34cdcd5e1233119b39492ba118
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-06-07 17:39:16 +0200

    Snack 7 da terminare (media voti)

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,7 +1,8 @@
 
 <?php
 
-
+/* Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta */
 
 
 
@@ -18,7 +19,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Snack-4</title>
 </head>
 <body>
   
